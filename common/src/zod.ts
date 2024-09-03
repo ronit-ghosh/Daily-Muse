159fc6f8de79e7eb108ea0d9dc81e65e92fb06da
ronit-ghosh <ronitghosh06@gmail.com>
2024-09-03 13:38:18 +0530

    Common Packages

---
+++ common/src/zod.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+export const signinValidation = z.object({
+    email: z.string().email({ message: "The email format is wrong!" }),
+    password: z.string().min(8, { message: "Password length must be more than 8!" }),
+});
+
+export const signupValidation = z.object({
+    username: z.string().min(3, { message: "Username length must be more than 3!" }),
+    email: z.string().email({ message: "The email format is wrong!" }),
+    password: z.string().min(8, { message: "Password length must be more than 8!" }),
+});
+
+export const createBlogValidation = z.object({
+    title: z.string(),
+    content: z.string()
+});
+
+export const updateBlogValidation = z.object({
+    id: z.string(),
+    title: z.string(),
+    content: z.string()
+});
+
+export type SigninValidation = z.infer<typeof signinValidation>
+export type SignupValidation = z.infer<typeof signupValidation>
+export type CreateBlogValidation = z.infer<typeof createBlogValidation>
+export type UpdateBlogValidation = z.infer<typeof updateBlogValidation>
